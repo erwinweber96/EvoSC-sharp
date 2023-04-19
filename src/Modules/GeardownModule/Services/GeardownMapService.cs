93a7c34b111413967b042191fd5ff498f24110b6
root <erwinweber96@live.com>
2023-04-19 11:32:40 +0000

    null externalid

--- src/Modules/GeardownModule/Services/GeardownMapService.cs
+++ src/Modules/GeardownModule/Services/GeardownMapService.cs
@@ -25,7 +25,7 @@ public class GeardownMapService
             MapName = mapInfoDto.GbxMapName,
             AuthorId = mapInfoDto.AuthorLogin,
             AuthorName = mapInfoDto.Username,
-            ExternalId = mapInfoDto.MapID.ToString(),
+            ExternalId = null,
             ExternalVersion = Convert.ToDateTime(mapInfoDto.UpdatedAt, NumberFormatInfo.InvariantInfo).ToUniversalTime(),
             ExternalMapProvider = MapProviders.ManiaExchange
         };
@@ -56,7 +56,7 @@ public class GeardownMapService
             MapName = mapInfoDto.GbxMapName,
             AuthorId = mapInfoDto.AuthorLogin,
             AuthorName = mapInfoDto.Username,
-            ExternalId = mapInfoDto.MapID.ToString(),
+            ExternalId = null,
             ExternalVersion = Convert.ToDateTime(mapInfoDto.UpdatedAt, NumberFormatInfo.InvariantInfo).ToUniversalTime(),
             ExternalMapProvider = MapProviders.ManiaExchange
         };
